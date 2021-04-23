c634ab4109cb69fd6fdc24e7b107a6d85cb55cb9
cwd-wenzhou <619715109@qq.com>
2021-04-23 17:27:35 +0800

    增加log内容

--- code/log/log.cpp
+++ code/log/log.cpp
@@ -1,8 +1,83 @@
 #include "log.h"
-log::log(/* args */)
+Log::Log(/* args */)
 {
 }
 
-log::~log()
+Log::~Log()
 {
+}
+
+int Log::GetLevel() {
+        lock_guard<mutex> locker(mtx_);
+        return level_;
+}
+
+void Log::SetLevel(int level) {
+        lock_guard<mutex> locker(mtx_);
+        level_ = level;
+}
+
+
+void Log::flush() {
+//     if(isAsync_) { 
+//         deque_->flush(); 
+//     }
+    fflush(fp_);
+}
+
+void Log::Init(int level,const char* path="./log",const char* suffix="WritenByCWD.log"){
+        level_=level;
+        IsOpen_=true;
+        path_=path;
+        suffix_=suffix;
+
+        time_t rawtime = time(nullptr);
+        struct tm *sysTime = localtime(&rawtime);
+        struct tm t = *sysTime;
+        //struct tm t=localtime(&rawtime);报错
+        Day_Since_Newyear=t.tm_yday;//记录今天
+        char fileName[LOG_NAME_LEN] = {0};
+        snprintf(fileName, LOG_NAME_LEN - 1, "%s/%04d_%02d_%02d%s", 
+            path_, t.tm_year + 1900, t.tm_mon + 1, t.tm_mday, suffix_);
+        //拼出fileName==./log/2021_04_23WritenByCWD.log 
+
+        {
+                lock_guard<mutex> locker(mtx_);
+                buff_.RetrieveAll();
+                if(fp_) { 
+                        flush();
+                        fclose(fp_); 
+                }
+                fp_ = fopen(fileName, "a");
+                if(fp_ == nullptr) {
+                        mkdir(path_, 0777);
+                        fp_ = fopen(fileName, "a");
+                } 
+                assert(fp_ != nullptr);
+        }
+}
+
+void Log::AppendLogLevelTitle_(int level) {
+        switch(level) {
+        case 0:
+                buff_.Append("[debug]: ", 9);
+                break;
+        case 1:
+                buff_.Append("[info] : ", 9);
+                break;
+        case 2:
+                buff_.Append("[warn] : ", 9);
+                break;
+        case 3:
+                buff_.Append("[error]: ", 9);
+                break;
+        default:
+                buff_.Append("[info] : ", 9);
+                break;
+        }
+}
+
+Log* Log::Instance() {
+    static Log inst;
+    return &inst;
 }
--- code/log/log.h
+++ code/log/log.h
@@ -5,22 +5,63 @@
  * 涉及知识点：
  *      1：#define 里定义函数用do {}while (0)使其成为一个语句块
  *      2：...为可变参数，#define 里的是这个## __VA_ARGS__
+ *      3:mkdir 在加入头文件<sys/stat.h>后可以直接调用，相当于在终端运行
+ *      4：fopen(filename,"a");
+ *                  "a": append: Open file for output at the end of a file. 
+ *                           Output operations always write data at the end of the file, expanding it. 
+ *      5:time:     
+ *             time(&timer);   get current time; same as: timer = time(NULL)  
+ *
  */ 
 #include <stdarg.h>     /* va_list, va_start, va_arg, va_end */
 #include <assert.h>
-
+#include <mutex>
+#include <thread>
+#include <sys/stat.h>         //mkdir
+#include <time.h>
+#include "../buffer/buffer.h"
+using namespace std;
 
 #define Log_Debug(format, ...) do {Log_Base(0,format,## __VA_ARGS__)} while(0);
 #define Log_Info(format, ...)      do {Log_Base(1,format,## __VA_ARGS__)} while(0);
 #define Log_Warn(format, ...)   do {Log_Base(2,format,## __VA_ARGS__)} while(0);
 #define Log_Error(format, ...)   do {Log_Base(3,format,## __VA_ARGS__)} while(0);
 
-class log
+class Log
 {
 private:
-        /* data */
+        static const int LOG_PATH_LEN = 256;
+        static const int LOG_NAME_LEN = 256;
+        static const int MAX_LINES = 50000;
+
+        int level_;//0,1,2,3指示日志紧急程度
+        mutex mtx_;
+        bool IsOpen_;//日志系统启动与否的标志
+
+        const char* path_;
+        const char* suffix_;
+
+        Buffer buff_;
+        FILE* fp_;
+        int Day_Since_Newyear;
+        //记录今天离元旦的距离，用于判断是否过了一天。过了一天了就要重开一个日志文件
+
+        void AppendLogLevelTitle_(int level);//贴每行日志的头
 public:
-        log(/* args */);
-        ~log();
+        Log(/* args */);
+        ~Log();
+
+        void Init(int level,const char* path="./log",const char* suffix="WritenByCWD.log");
+        Log* Instance();//饿汉单例模式
+        void SetLevel(int level);
+        int GetLevel();
+
+        void write();
+
+        void flush();
+
+        bool IsOpen(){return IsOpen_;}//返回日志系统是否已经启动
+
+
 };
 
